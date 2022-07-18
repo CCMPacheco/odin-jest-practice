05112c0a40c9c1dcc6a80bce790da710fbb60588
CCMPacheco <pachecoccmp@gmail.com>
2022-07-17 21:27:13 -0300

    Caesar cipher test passed

---
+++ src/caesar-cipher.js
@@ -0,0 +1,51 @@
+function caesarCipher(string, key) {
+  if (typeof string !== "string") return "Invalid input";
+  if (isNaN(key)) return "Invalid key";
+  if (!Number.isInteger(key)) return "Invalid key";
+  if (key < 0) return "Invalid key";
+
+  let cipherText = "";
+
+  for (const char of string) {
+    cipherText += rotate(char, key);
+  }
+  return cipherText;
+}
+
+function rotate(char, key) {
+  if (!char.match(/[a-z]/i)) return char;
+  if (isLower(char)) {
+    let cipher = (char.charCodeAt() + key) % 123;
+    if (cipher > 96 && cipher < 123) {
+      return String.fromCharCode(cipher);
+    } else {
+      while (cipher < 0 || cipher > 25) {
+        cipher = cipher % 26;
+      }
+      cipher += 97;
+      return String.fromCharCode(cipher);
+    }
+  }
+  if (isUpper(char)) {
+    let cipher = (char.charCodeAt() + key) % 91;
+    if (cipher > 64 && cipher < 91) {
+      return String.fromCharCode(cipher);
+    } else {
+      while (cipher < 0 || cipher > 25) {
+        cipher = cipher % 26;
+      }
+      cipher += 65;
+      return String.fromCharCode(cipher);
+    }
+  }
+}
+
+function isLower(char) {
+  return /[a-z]/.test(char);
+}
+
+function isUpper(char) {
+  return /[A-Z]/.test(char);
+}
+
+export default caesarCipher;
---
+++ src/caesar-cipher.test.js
@@ -0,0 +1,53 @@
+import caesarCipher from "./caesar-cipher";
+
+test("check key positive integer", () => {
+  expect(caesarCipher("defend the east wall of the castle", 1)).toBe(
+    "efgfoe uif fbtu xbmm pg uif dbtumf"
+  );
+});
+
+test("check key negative integer", () => {
+  expect(caesarCipher("abcdefghijklmnopqrstuvwxyz", -1)).toBe("Invalid key");
+});
+
+test("check key float", () => {
+  expect(caesarCipher("abcdefghijklmnopqrstuvwxyz", 1.5)).toBe("Invalid key");
+});
+
+test("check input is string", () => {
+  expect(caesarCipher(123, 1)).toBe("Invalid input");
+});
+
+test("check key equals to 0", () => {
+  expect(caesarCipher("abcdefghijklmnopqrstuvwxyz", 0)).toBe(
+    "abcdefghijklmnopqrstuvwxyz"
+  );
+});
+
+test("String upper and lower case", () => {
+  expect(caesarCipher("AbCdeFgH", 20)).toBe("UvWxyZaB");
+});
+
+test("String upper case", () => {
+  expect(caesarCipher("ABCDEFGH", 20)).toBe("UVWXYZAB");
+});
+
+test("String lower case", () => {
+  expect(caesarCipher("abcdefgh", 20)).toBe("uvwxyzab");
+});
+
+test("String only symbols", () => {
+  expect(caesarCipher("@#$%^&*", 20)).toBe("@#$%^&*");
+});
+
+test("String numbers, letters, upper case and lower case, and symbols", () => {
+  expect(caesarCipher("123abcDEFG@#$%^&*", 20)).toBe("123uvwXYZA@#$%^&*");
+});
+
+test("check null key", () => {
+  expect(caesarCipher("123abcDEFG@#$%^&*", null)).toBe("Invalid key");
+});
+
+test("check null input and null key", () => {
+  expect(caesarCipher(null, null)).toBe("Invalid input");
+});
